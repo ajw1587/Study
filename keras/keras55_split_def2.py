aec69f585fe063a65670129dffdaa3daaaf91d7e
ajw1587 <dlsdnghkd0503@naver.com>
2021-01-18 20:31:37 +0900

    Add optimizer,Reduce_LR

--- keras/keras55_split_def2.py
+++ keras/keras55_split_def2.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # 리스트 -> 1차원 행열
-a = np.array(range(1, 11))      # 1~10
+a = np.array(range(1, 21))      # 1~10
 size = 6
 def split_x(seq, size):
     aaa = []
@@ -9,19 +9,21 @@ def split_x(seq, size):
         subset = seq[i : (i+size)]
         # aaa.append([item for item in subset])
         aaa.append(subset)
-    print(aaa)
     return np.array(aaa)
 
 # 2차원 행열 -> 3차원 행열
 def split_xy2(dataset, x_low, x_col, y_low, y_col):
-    x = []
-    y = []
-    for i in range(len(dataset) - x_low +1):
+    x, y = [], []
+    for i in range(len(dataset) - x_low -1):
+        if (i + x_low) > len(dataset):
+            break
         x_subset = dataset[i : i + x_low, 0 : x_col]
         x.append(x_subset)
 
     for i in range(len(dataset) - y_low -1):
-        y_subset = dataset[i + x_low, -y_col:]
+        if (i + x_low + y_low) > len(dataset):
+            break
+        y_subset = dataset[i + x_low : i + x_low + y_low , -y_col:]
         y.append(y_subset)
 
     x = np.array(x)
@@ -29,7 +31,8 @@ def split_xy2(dataset, x_low, x_col, y_low, y_col):
     return x, y
 
 dataset = split_x(a, size)
-x, y = split_xy2(dataset, 2, 3, 1, 3)
+print(dataset.shape)
+x, y = split_xy2(dataset, 3, 3, 2, 4)
 print(dataset)
 print(x)
 print(x.shape)
