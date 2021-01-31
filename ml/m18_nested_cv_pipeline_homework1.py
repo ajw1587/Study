06fb190943015f64257f0b699c800a7c647e1fe2
ajw1587 <dlsdnghkd0503@naver.com>
2021-01-31 23:15:00 +0900

    Add HomeWork

--- ml/m18_nested_cv_pipeline_homework1.py
+++ ml/m18_nested_cv_pipeline_homework1.py
@@ -5,6 +5,7 @@ from sklearn.datasets import load_wine
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, KFold
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.pipeline import Pipeline
 
 # 1. 데이터
 dataset = load_wine()
@@ -14,9 +15,21 @@ y = dataset.target
 
 # 2. 모델
 parameters = [
-    {'forest__n_estimators': [100, 200]},
-    {'forest__max_depth': [6, 8, 10, 12]},
-    {'forest__min_samples_leaf': [3, 5, 6, 10]},
-    {'forest__min_samples_split': [2, 3, 5, 10]},
-    {'forest__n_jobs': [-1, 2, 4]}
+    {'forest__n_estimators': [100]},              # , 200
+    {'forest__max_depth': [6]},                   # , 8, 10, 12
+    {'forest__min_samples_leaf': [3]},            # , 5, 6, 10
+    {'forest__min_samples_split': [2]},           # , 3, 5, 10
+    {'forest__n_jobs': [-1]}                      # , 2, 4
 ]
+kfold = KFold(n_splits = 5, shuffle = True)
+
+model1 = RandomizedSearchCV(RandomForestClassifier(), parameters, cv = kfold)
+model2 = Pipeline([('scaler', StandardScaler()), ('forest', model1)])
+model3 = RandomizedSearchCV(model2, parameters, cv = kfold)
+
+# 3. Fit
+model3.fit(x, y)
+
+# 4. Score
+acc_result = model3.score(x, y)
+print(acc_result)
