41e114efba6d287530a1efe3a75421a8e40cfe5a
ajw1587 <dlsdnghkd0503@naver.com>
2021-02-26 21:21:28 +0900

    Add Dacon

--- test/Dacon_Computer_Vision2_04_denoising.py
+++ test/Dacon_Computer_Vision2_04_denoising.py
@@ -4,29 +4,38 @@ import numpy as np
 import pandas as pd
 
 '''
-filepath = '../data/csv/Computer_Vision2/train_dirty_mnist_2nd/00001.png'
+filepath = '../data/csv/Computer_Vision2/train_dirty_mnist_2nd/00009.png'
 img = cv.imread(filepath)
 img2 = img.copy()
 img2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
-img2 = cv.resize(img2, dsize = (256, 256))
+img2 = cv.resize(img2, dsize = (128, 128))
 
-# 2차 노이즈 제거
-kernel = np.ones((2, 2), np.uint8)
-img2 = cv.dilate(img2, kernel, iterations = 1)
 
 # 임계처리
-_, img2 = cv.threshold(img2, 254, 255, cv.THRESH_BINARY)
+_, img2 = cv.threshold(img2, 173, 255, cv.THRESH_BINARY)
+
+# 2차 노이즈 제거
+kernel = np.ones((1, 1), np.uint8)
+img2 = cv.dilate(img2, kernel, iterations = 1)
+# img2 = cv.morphologyEx(img2, cv.MORPH_OPEN, np.ones((1, 1), np.uint8), iterations = 5)
 
 # Blur 처리
+# im2 = cv.GaussianBlur(img2, (5, 5), 0)
+img2 = cv.medianBlur(img2, ksize = 3)
 img2 = cv.medianBlur(img2, ksize = 3)
 
-img2 = np.array(img2)
-img2 = img2.reshape(256, 256, 1)
+# kernel = np.ones((2, 2), np.uint8)
+# _, img2 = cv.threshold(img2, 170, 255, cv.THRESH_BINARY)
+# img2 = cv.dilate(img2, kernel)
+# img2 = cv.morphologyEx(img2, cv.MORPH_CLOSE, kernel)
+# img2 = cv.morphologyEx(img2, cv.MORPH_OPEN, kernel)
+# img2 = cv.bilateralFilter(img2, 10, 50, 50)
 
-plt.figure(figsize = (20, 10))
+plt.figure(figsize = (10, 5))
 plt.subplot(121), plt.imshow(img)
 plt.subplot(122), plt.imshow(img2)
 plt.show()
+
 
 
 ##################################################################################
@@ -46,17 +55,20 @@ for i in range(50000):
     
     # Noise 제거
     subset = cv.cvtColor(subset, cv.COLOR_BGR2GRAY)
-    subset = cv.resize(subset, dsize = (256, 256))
-
-    # 2차 노이즈 제거
-    kernel = np.ones((2, 2), np.uint8)
-    subset = cv.dilate(subset, kernel, iterations = 1)
+    subset = cv.resize(subset, dsize = (128, 128))
 
     # 임계처리
-    _, subset = cv.threshold(subset, 254, 255, cv.THRESH_BINARY)
+    _, img2 = cv.threshold(img2, 173, 255, cv.THRESH_BINARY)
+
+    # 2차 노이즈 제거
+    kernel = np.ones((1, 1), np.uint8)
+    img2 = cv.dilate(img2, kernel, iterations = 1)
+    # img2 = cv.morphologyEx(img2, cv.MORPH_OPEN, np.ones((1, 1), np.uint8), iterations = 5)
 
     # Blur 처리
-    subset = cv.medianBlur(subset, ksize = 3)
+    # im2 = cv.GaussianBlur(img2, (5, 5), 0)
+    img2 = cv.medianBlur(img2, ksize = 3)
+    img2 = cv.medianBlur(img2, ksize = 3)
 
     # # resize (50000,256,256) 너무 크다 줄이자!
     ## subset = cv2.resize(subset, dsize = (64, 64), interpolation = cv2.INTER_LINEAR)
@@ -80,17 +92,20 @@ for i in range(5000):
     
     # Noise 제거
     subset = cv.cvtColor(subset, cv.COLOR_BGR2GRAY)
-    subset = cv.resize(subset, dsize = (256, 256))
-
-    # 2차 노이즈 제거
-    kernel = np.ones((2, 2), np.uint8)
-    subset = cv.dilate(subset, kernel, iterations = 1)
+    subset = cv.resize(subset, dsize = (128, 128))
 
     # 임계처리
-    _, subset = cv.threshold(subset, 254, 255, cv.THRESH_BINARY)
-
+    _, img2 = cv.threshold(img2, 173, 255, cv.THRESH_BINARY)
+    
+    # 2차 노이즈 제거
+    kernel = np.ones((1, 1), np.uint8)
+    img2 = cv.dilate(img2, kernel, iterations = 1)
+    # img2 = cv.morphologyEx(img2, cv.MORPH_OPEN, np.ones((1, 1), np.uint8), iterations = 5)
+    
     # Blur 처리
-    subset = cv.medianBlur(subset, ksize = 3)
+    # im2 = cv.GaussianBlur(img2, (5, 5), 0)
+    img2 = cv.medianBlur(img2, ksize = 3)
+    img2 = cv.medianBlur(img2, ksize = 3)
 
     # resize
     # subset = cv2.resize(subset, dsize = (64, 64), interpolation = cv2.INTER_LINEAR)
@@ -102,79 +117,109 @@ for i in range(5000):
 train_img = np.array(train_img)
 test_img = np.array(test_img)
 
+train_img = train_img/255.
+test_img = test_img/255.
+
 # numpy 저장
-np.save('../data/csv/Computer_Vision2/numpy_file/Train_Computer_Vision2_denoising.npy',
+np.save('../data/csv/Computer_Vision2/numpy_file/Train_Computer_Vision2_denoising128.npy',
         train_img)
-np.save('../data/csv/Computer_Vision2/numpy_file/Test_Computer_Vision2_denoising.npy',
+np.save('../data/csv/Computer_Vision2/numpy_file/Test_Computer_Vision2_denoising128.npy',
         test_img)
+
 '''
 ############################################################################################
 # load data
-x_train = np.load('../data/csv/Computer_Vision2/numpy_file/Train_Computer_Vision2_denoising.npy')
-x_test = np.load('../data/csv/Computer_Vision2/numpy_file/Test_Computer_Vision2_denoising.npy')
+x_train = np.load('../data/csv/Computer_Vision2/numpy_file/Train_Computer_Vision2_denoising128.npy')
+x_test = np.load('../data/csv/Computer_Vision2/numpy_file/Test_Computer_Vision2_denoising128.npy')
 y_train = pd.read_csv('../data/csv/Computer_Vision2/train_dirty_mnist_2nd_answer.csv')
 y_submission = pd.read_csv('../data/csv/Computer_Vision2/sample_submission.csv')
 
-x_train = x_train.reshape(50000, 256, 256, 1)
-x_test = x_test.reshape(5000, 256, 256, 1)
-y_train = y_train.iloc[:, 1:].values
-y_submission = y_submission.iloc[:, 1:].values
+x_train = x_train.reshape(50000, 128, 128, 1)
+x_test = x_test.reshape(5000, 128, 128, 1)
 
 # train_split_data
+from sklearn.model_selection import train_test_split
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, train_size = 0.8, random_state = 77)
 
-
-# print(x_train.shape)        (50000, 256, 256, 1)
-# print(x_test.shape)         (5000, 256, 256, 1)
-# print(y_train.shape)        (50000, 26)
-# print(y_submission.shape)   (5000, 26)
-# print(type(x_train))        <class 'numpy.ndarray'>
-# print(type(y_train))        <class 'numpy.ndarray'>
+print(x_train.shape)        # (50000, 128, 128, 1)
+print(x_test.shape)         # (5000, 128, 128, 1)
+print(y_train.shape)        # (50000, 26)
+print(y_submission.shape)   # (5000, 26)
+print(type(x_train))        # <class 'numpy.ndarray'>
+print(type(y_train))        # <class 'numpy.ndarray'>
 
 # model
-from tensorflow.keras.models import Model
+from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, Input, Activation, BatchNormalization, MaxPooling2D
 from tensorflow.keras.optimizers import Adam
-opt = Adam(learning_rate = 1e-3)
+opt = Adam(learning_rate = 0.001)
 
 def my_model():
-    input1 = Input(shape = (256, 256, 1))
+    input1 = Input(shape = (128, 128, 1))
     x = Conv2D(32, (3, 3), padding = 'same')(input1)
     x = Activation('relu')(x)
     x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size = (3,3))(x)
     x = Dropout(0.25)(x)
     
-    x = Conv2D(64, (3, 3), padding = 'same')(input1)
-    x = Activation('relu')(x)
-    x = BatchNormalization()(x)
+    # x = Conv2D(64, (3, 3), padding = 'same')(input1)
+    # x = Activation('relu')(x)
+    # x = BatchNormalization()(x)
     x = Conv2D(64, (3, 3), padding = 'same')(input1)
     x = Activation('relu')(x)
     x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size = (2,2))(x)
     x = Dropout(0.25)(x)
 
-    x = Conv2D(128, (3, 3), padding = 'same')(input1)
-    x = Activation('relu')(x)
-    x = BatchNormalization()(x)
-    x = Conv2D(128, (3, 3), padding = 'same')(input1)
-    x = Activation('relu')(x)
-    x = BatchNormalization()(x)
-    x = MaxPooling2D(pool_size = (2,2))(x)
-    x = Dropout(0.25)(x)
+    # x = Conv2D(128, (3, 3), padding = 'same')(input1)
+    # x = Activation('relu')(x)
+    # x = BatchNormalization()(x)
+    # x = Conv2D(128, (3, 3), padding = 'same')(input1)
+    # x = Activation('relu')(x)
+    # x = BatchNormalization()(x)
+    # x = MaxPooling2D(pool_size = (2,2))(x)
+    # x = Dropout(0.25)(x)
 
     x = Flatten()(x)
-    x = Dense(256)(x)
+    x = Dense(128)(x)
     x = Activation('relu')(x)
     x = BatchNormalization()(x)
     x = Dropout(0.5)(x)
 
-    x = Dense(26)(x)
-    output1 = Activation('softmax')(x)
+    x = Dense(1)(x)
+    output1 = Activation('sigmoid')(x)
     model = Model(inputs = input1, outputs = output1)
 
-    model.compile(loss = 'categorical_crossentropy', optimizer = opt, metrics = ['accuracy'])
+    model.compile(loss = 'binary_crossentropy', optimizer = opt, metrics = ['accuracy'])
 
     return model
 
+
+
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+es = EarlyStopping(monitor = 'val_loss', patience = 300, mode = 'auto')
+reduce_lr = ReduceLROnPlateau(monitor = 'val_loss', factor = 0.3, patience = 150, mode = 'auto')
+
 model = my_model()
-model.fit(x_train, y_train, epochs = 1, validate_)
+
+from string import ascii_lowercase
+alpha = ascii_lowercase     # 알파벳 리스트
+for i in alpha:
+    cp_path = '../data/modelcheckpoint/Computer_Vision2/Dacon_Computer_Vision2_denoising128_' + str(i) + '.hdf5'
+    cp = ModelCheckpoint(cp_path, save_best_only = True, mode = 'auto')
+
+    # y_train, y_val에서 필요한 alphabet 추출
+    y_train_alpha = y_train.loc[:, i].values
+    y_val_alpha = y_val.loc[:, i].values
+
+    hist = model.fit(x_train, y_train_alpha, epochs = 1000, validation_data = (x_val, y_val_alpha),
+                     callbacks = [es, cp, reduce_lr])
+
+    # 6. Predict
+    model2 = load_model(cp_path)
+    y_pred = model2.predict(x_test)
+    y_pred = np.where(y_pred < 0.5, 0, 1)
+    print(y_pred)
+    print(y_pred.shape)
+    y_submission.loc[:, i] = y_pred
+    y_submission.to_csv('../data/modelcheckpoint/Computer_Vision2/Computer_Vision2_submission_denoising128.csv', index = False)
