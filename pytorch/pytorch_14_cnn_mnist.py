b33252f072f1598971254189c791ef0a02557512
ajw1587 <dlsdnghkd0503@naver.com>
2021-03-28 22:02:28 +0900

    Add pytorch

--- pytorch/pytorch_14_cnn_mnist.py
+++ pytorch/pytorch_14_cnn_mnist.py
@@ -1,3 +1,5 @@
+# https://wikidocs.net/63618
+
 import torch
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
@@ -10,7 +12,7 @@ torch.manual_seed(777)
 
 # GPU 사용 가능할 경우 Seed 고정
 if device == 'cuda':
-    torch.cuda.manaul_seed_all(777)
+    torch.cuda.manual_seed_all(777)
 
 learning_rate = 0.001
 training_epochs = 15
@@ -42,13 +44,13 @@ class CNN(torch.nn.Module):
         # First Layer
         self.layer1 = torch.nn.Sequential(
             torch.nn.Conv2d(1, 32, kernel_size = 3, stride = 1, padding = 1),
-            torch.nn.Relu(),
+            torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size = 2, stride = 2))
         
         # Second Layer
         self.layer2 = torch.nn.Sequential(
             torch.nn.Conv2d(32, 64, kernel_size = 3, stride = 1, padding = 1),
-            torch.nn.Relu(),
+            torch.nn.ReLU(),
             torch.nn.MaxPool2d(kernel_size = 2, stride = 2))
         
         # 전결합층
@@ -60,4 +62,37 @@ class CNN(torch.nn.Module):
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
-        out = self.view(out.size(0), -1)    # Flatten
+        out = out.view(out.size(0), -1)    # Flatten
+        out = self.fc(out)
+        return out
+
+model = CNN().to(device)
+
+# 비용 함수와 옵티마이저 정의
+criterion = torch.nn.CrossEntropyLoss().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+
+# 총 배치의 수 출력
+total_batch = len(data_loader)
+print('총 배치수: {}'.format(total_batch))
+
+for epoch in range(training_epochs):
+    avg_cost = 0
+    # i = 0
+
+    for X, Y in data_loader:                # X는 미니 배치, Y는 레이블 600번 반복
+
+        X = X.to(device)
+        Y = Y.to(device)
+
+        optimizer.zero_grad()
+        hypothesis = model(X)
+        cost = criterion(hypothesis, Y)
+        cost.backward()
+        optimizer.step()
+        avg_cost += cost/total_batch        # 600번 반복으로 인한 나누기
+        # print(i)
+        # i += 1
+    
+    print('[Epoch: {:>4}] cost = {:>.9}'.format(epoch + 1, avg_cost))
+    
