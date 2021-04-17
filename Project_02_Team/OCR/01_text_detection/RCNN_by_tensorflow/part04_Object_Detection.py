79f364429272b1c4e5058072124089dbb37591a7
ajw1587 <dlsdnghkd0503@naver.com>
2021-04-17 21:29:51 +0900

    Add Object Detection

---
+++ Project_02_Team/OCR/01_text_detection/RCNN_by_tensorflow/part04_Object_Detection.py
@@ -0,0 +1,8 @@
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.applications import imagenet_utils
+from tensorflow.keras.preprocessing.image import img_to_array
+from imutils.object_detection import non_max_suppression
+import numpy as np
+import argparse
+import cv2
