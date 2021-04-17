c6930c7f31c92b7e82bf942698202a4c857859ae
ajw1587 <dlsdnghkd0503@naver.com>
2021-04-17 21:32:59 +0900

    Add Object Detection

--- Project_02_Team/OCR/01_text_detection/RCNN_by_tensorflow/part04_Object_Detection.py
+++ Project_02_Team/OCR/01_text_detection/RCNN_by_tensorflow/part04_Object_Detection.py
@@ -6,3 +6,33 @@ from imutils.object_detection import non_max_suppression
 import numpy as np
 import argparse
 import cv2
+
+def selective_search(image, method="fast"):
+	# initialize OpenCV's selective search implementation and set the
+	# input image
+	ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
+	ss.setBaseImage(image)
+	# check to see if we are using the *fast* but *less accurate* version
+	# of selective search
+	if method == "fast":
+		ss.switchToSelectiveSearchFast()
+	# otherwise we are using the *slower* but *more accurate* version
+	else:
+		ss.switchToSelectiveSearchQuality()
+	# run selective search on the input image
+	rects = ss.process()
+	# return the region proposal bounding boxes
+	return rects
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the input image")
+ap.add_argument("-m", "--method", type=str, default="fast",
+	choices=["fast", "quality"],
+	help="selective search method")
+ap.add_argument("-c", "--conf", type=float, default=0.9,
+	help="minimum probability to consider a classification/detection")
+ap.add_argument("-f", "--filter", type=str, default=None,
+	help="comma separated list of ImageNet labels to filter on")
+args = vars(ap.parse_args())
