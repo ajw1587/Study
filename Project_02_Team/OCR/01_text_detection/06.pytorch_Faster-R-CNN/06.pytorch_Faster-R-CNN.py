3c3f6038a5ed140cc27494c7606d7c150c07a015
ajw1587 <dlsdnghkd0503@naver.com>
2021-04-24 13:48:59 +0900

    Add

--- Project_02_Team/OCR/01_text_detection/06.pytorch_Faster-R-CNN/06.pytorch_Faster-R-CNN.py
+++ Project_02_Team/OCR/01_text_detection/06.pytorch_Faster-R-CNN/06.pytorch_Faster-R-CNN.py
@@ -5,6 +5,7 @@ import torch
 import torchvision
 import torch.nn as nn
 import cv2 as cv2
+import torch.nn.functional as F
 from torchvision import transforms
 import matplotlib.pyplot as plt
 import numpy as np
@@ -237,20 +238,20 @@ print('gt_argmax_ious: ', gt_argmax_ious)
 
 # what ground truth bbox is associated with each anchor box
 argmax_ious = ious.argmax(axis=1)
-print('argmax_ious.shape: ', argmax_ious.shape)
+print('LINE 240 argmax_ious.shape: ', argmax_ious.shape)
 print(ious)
-print('argmax_ious: ', argmax_ious)
+print('LINE 242 argmax_ious: ', argmax_ious)
 
 max_ious = ious[np.arange(len(index_inside)), argmax_ious]
-print('max_ious.shape: ', max_ious.shape)
-print('max_ious: ', max_ious)
+print('LINE 245 max_ious.shape: ', max_ious.shape)
+print('LINE 246 max_ious: ', max_ious)
 
 # Label
 # set the labels of 8940 valid anchor boxes to -1(ignore)
 label = np.empty((len(index_inside),), dtype=np.int32)
 label.fill(-1)
-print('label.shape: ', label.shape)
-print('label: ', label)
+print('LINE 252 label.shape: ', label.shape)
+print('LINE 253 label: ', label)
 
 
 
@@ -291,16 +292,18 @@ if len(neg_index) > n_neg:
                                     replace = False)
     label[disable_index] = -1
 
+
 # convert the format of valid anchor boxes [x1, y1, x2, y2]
 # For each valid anchor box, find the groundtruth object which has max_iou
-print('argmax_ious: ', argmax_ious)
-print('argmax_ious.shape: ', argmax_ious.shape)
-print('argmax_ious.type: ', type(argmax_ious))
-print('bbox.type: ', type(bbox))
+print('LINE 297 argmax_ious: ', argmax_ious)
+print('LINE 298 argmax_ious.shape: ', argmax_ious.shape)
+print('LINE 299 argmax_ious.type: ', type(argmax_ious))
+print('LINE 300 bbox.type: ', type(bbox))
 bbox = np.array(bbox)
-print('bbox.type: ', type(bbox))
+print('LINE 302 bbox.type: ', type(bbox))
 max_iou_bbox = bbox[argmax_ious]
-print(max_iou_bbox.shape)
+print('LINE 304 max_iou_bbox.shape: ', max_iou_bbox.shape)
+print('LINE 305 max_iou_bbox: ', max_iou_bbox)
 
 height = valid_anchor_boxes[:, 3] - valid_anchor_boxes[:, 1]
 width = valid_anchor_boxes[:, 2] = valid_anchor_boxes[:, 0]
@@ -322,9 +325,493 @@ dh = np.log(base_height / height)
 dw = np.log(base_width / width)
 
 anchor_locs = np.vstack((dx, dy, dw, dh))
-print(anchor_locs.shape)
+print('LINE 327 anchor_locs: ', anchor_locs)
 anchor_locs = anchor_locs.transpose()
-print(anchor_locs.shape)
+print('LINE 329 anchor_locs.shape: ', anchor_locs.shape)
+
+
+# First set the label=-1 and locations=0 of the 22500 anchor boxes, 
+# and then fill in the locations and labels of the 8940 valid anchor boxes
+# NOTICE: For each training epoch, we randomly select 128 positive + 128 negative 
+# from 8940 valid anchor boxes, and the others are marked with -1
+anchor_labels = np.empty((len(anchor_boxes),), dtype=label.dtype)
+anchor_labels.fill(-1)
+anchor_labels[index_inside] = label
+print('LINE 339 anchor_labels.shape: ', anchor_labels.shape)
+print('LINE 340 anchor_labels[:10]: ', anchor_labels[:10])
+
+anchor_locations = np.empty((len(anchor_boxes),) + anchor_boxes.shape[1:], dtype=anchor_locs.dtype)
+anchor_locations.fill(0)
+anchor_locations[index_inside, :] = anchor_locs
+print('LINE 345 anchor_locations.shape: ', anchor_locations.shape)
+print('LINE 346 anchor_locations[:10, :]: ', anchor_locations[:10, :])
+
 
 
 ########################### RPN
+# 1) Define RPN
+# Send the features of the input image to the Region Proposal Network (RPN), 
+# predict 22500 region proposals (ROIs)
+in_channels = 512
+mid_channels = 512
+n_anchor = 9
+
+conv1 = nn.Conv2d(in_channels, mid_channels, 3, 1, 1).to(DEVICE)
+conv1.weight.data.normal_(0, 0.01)
+conv1.bias.data.zero_()
+
+# bounding box regressor
+reg_layer = nn.Conv2d(mid_channels, n_anchor * 4, 1, 1, 0).to(DEVICE)
+reg_layer.weight.data.normal_(0, 0.01)
+reg_layer.bias.data.zero_()
+
+# classifier(object or not)
+cls_layer = nn.Conv2d(mid_channels, n_anchor * 2, 1, 1, 0).to(DEVICE)
+cls_layer.weight.data.normal_(0, 0.01)
+cls_layer.bias.data.zero_()
+
+# 2) Classifiaction and Bounding box regression
+x = conv1(output_map.to(DEVICE)) # output_map = faster_rcnn_feature_extractor(imgTensor)
+pred_anchor_locs = reg_layer(x) # bounding box regresor output
+pred_cls_scores = cls_layer(x)  # classifier output 
+
+print('LINE 377 pred_anchor_locs.shape: ', pred_anchor_locs.shape)
+print('LINE 378 pred_cls_scores.shape: ', pred_cls_scores.shape)
+
+# Convert RPN to predict the position and classification format of the anchor box
+# Position: [1, 36(9*4), 50, 50] => [1, 22500(50*50*9), 4] (dy, dx, dh, dw) 
+# Classification: [1, 18(9*2), 50, 50] => [1, 22500, 2] (1, 0)
+pred_anchor_locs = pred_anchor_locs.permute(0, 2, 3, 1).contiguous().view(1, -1, 4)
+print('LINE 384 pred_anchor_locs.shape: ', pred_anchor_locs.shape)
+
+pred_cls_scores = pred_cls_scores.permute(0, 2, 3, 1).contiguous()
+print('LINE 387 pred_anchor_locs.shape: ', pred_cls_scores.shape)
+
+objectness_score = pred_cls_scores.view(1, 50, 50, 9, 2)[:, :, :, :, 1].contiguous().view(1, -1)
+print('LINE 390 objectness_score.shape: ', objectness_score.shape)
+
+pred_cls_scores = pred_cls_scores.view(1, -1, 2)
+print('LINE 393 pred_cls_scores.shape: ', pred_cls_scores.shape)
+
+# According to the 22500 ROIs predicted by RPN and 22500 anchor boxes, 
+# calculate the RPN loss
+print('LINE 397 pred_anchor_locs.shape: ', pred_anchor_locs.shape)
+print('LINE 398 pred_cls_scores.shape: ', pred_cls_scores.shape)
+print('LINE 399 anchor_locations.shape: ', anchor_locations.shape)
+print('LINE 400 anchor_labels.shape: ', anchor_labels.shape)
+
+rpn_loc = pred_anchor_locs[0]
+rpn_score = pred_cls_scores[0]
+
+gt_rpn_loc = torch.from_numpy(anchor_locations)
+gt_rpn_score = torch.from_numpy(anchor_labels)
+
+print(rpn_loc.shape, rpn_score.shape,
+      gt_rpn_loc.shape, gt_rpn_score.shape)
+
+
+# 3) Multi-task loss
+# For classification we use cross-entropy loss
+rpn_cls_loss = F.cross_entropy(rpn_score, gt_rpn_score.long().to(DEVICE), ignore_index = -1)
+print(rpn_cls_loss)
+
+# only positive samples
+pos = gt_rpn_score > 0
+mask = pos.unsqueeze(1).expand_as(rpn_loc)
+print(mask.shape)
+
+# take those bounding boxes whick have positive labels
+mask_loc_preds = rpn_loc[mask].view(-1, 4)
+mask_loc_targets = gt_rpn_loc[mask].view(-1, 4)
+print(mask_loc_preds.shape, mask_loc_targets.shape)
+
+x = torch.abs(mask_loc_targets.cpu() - mask_loc_preds.cpu())
+rpn_loc_loss = ((x < 1).float() * 0.5 * x ** 2) + ((x >= 1).float() * (x - 0.5))
+print(rpn_loc_loss.sum())
+
+# Combining both the rpn_cls_loss and rpn_reg_loss
+
+rpn_lambda = 10
+N_reg = (gt_rpn_score > 0).float().sum()
+rpn_loc_loss = rpn_loc_loss.sum() / N_reg
+rpn_loss = rpn_cls_loss + (rpn_lambda * rpn_loc_loss)
+print(rpn_loss)
+
+
+# Proposal layer
+# 1) Transform anchor boxes
+# Send the 22500 ROIs predicted by RPN to Fast RCNN to predict bbox + classifications
+# First use NMS (Non-maximum supression) to reduce 22500 ROI to 2000
+nms_thresh = 0.7  # non-maximum supression (NMS) 
+n_train_pre_nms = 12000 # no. of train pre-NMS
+n_train_post_nms = 2000 # after nms, training Fast R-CNN using 2000 RPN proposals
+n_test_pre_nms = 6000
+n_test_post_nms = 300 # During testing we evaluate 300 proposals,
+min_size = 16
+
+# the labelled 22500 anchor boxes
+# format converted from [x1, y1, x2, y2] to [ctrx, ctry, w, h]
+anc_height = anchor_boxes[:, 3] - anchor_boxes[:, 1]
+anc_width = anchor_boxes[:, 2] - anchor_boxes[:, 0]
+anc_ctr_y = anchor_boxes[:, 1] + 0.5 * anc_height
+anc_ctr_x = anchor_boxes[:, 0] + 0.5 * anc_width
+print(anc_ctr_x.shape)
+
+# The 22500 anchor boxes location and labels predicted by RPN (convert to numpy)
+# format = (dx, dy, dw, dh)
+pred_anchor_locs_numpy = pred_anchor_locs[0].cpu().data.numpy()
+objectness_score_numpy = objectness_score[0].cpu().data.numpy()
+
+dy = pred_anchor_locs_numpy[:, 1::4]
+dx = pred_anchor_locs_numpy[:, 0::4]
+dh = pred_anchor_locs_numpy[:, 3::4]
+dw = pred_anchor_locs_numpy[:, 2::4]
+print(dy.shape)
+
+# ctr_y = dy predicted by RPN * anchor_h + anchor_cy
+# ctr_x similar
+# h = exp(dh predicted by RPN) * anchor_h
+# w similar
+ctr_y = dy * anc_height[:, np.newaxis] + anc_ctr_y[:, np.newaxis]
+ctr_x = dx * anc_width[:, np.newaxis] + anc_ctr_x[:, np.newaxis]
+h = np.exp(dh) * anc_height[:, np.newaxis]
+w = np.exp(dw) * anc_width[:, np.newaxis]
+print(w.shape)
+
+roi = np.zeros(pred_anchor_locs_numpy.shape, dtype=anchor_locs.dtype)
+roi[:, 0::4] = ctr_x - 0.5 * w
+roi[:, 1::4] = ctr_y - 0.5 * h
+roi[:, 2::4] = ctr_x + 0.5 * w
+roi[:, 3::4] = ctr_y + 0.5 * h
+
+print(roi.shape)
+
+# 2) Clip the anchor boxes to the image
+img_size = (800, 800)
+roi[:, slice(0, 4, 2)] = np.clip(roi[:, slice(0, 4, 2)], 0, img_size[0]) # [:, 0, 2]
+roi[:, slice(1, 4, 2)] = np.clip(roi[:, slice(1, 4, 2)], 0, img_size[1]) # [:, 1, 3]
+
+print(roi.shape, np.max(roi), np.min(roi))
+
+# remove predicted boxes with either height or width < threshold
+hs = roi[:, 3] - roi[:, 1]
+ws = roi[:, 2] - roi[:, 0]
+
+keep = np.where((hs >= min_size) & (ws >= min_size))[0]
+roi = roi[keep, :]
+score = objectness_score_numpy[keep]
+print(keep.shape, roi.shape, score.shape)
+
+
+# 3) Select top-12000 anchor boxes by objectness score
+# sort all (proposal, score) pairs by score from highest to lowest
+order = score.ravel().argsort()[::-1]
+print(order.shape)
+
+# take top pre_nms_topN (e.g. 12000 while training and 300 while testing)
+order = order[:n_train_pre_nms]
+roi = roi[order, :]
+print(order.shape, roi.shape)
+
+# 4) Non maximum suppression(select 2000 bounding boxes)
+# take all the roi boxes
+x1 = roi[:, 0]
+y1 = roi[:, 1]
+x2 = roi[:, 2]
+y2 = roi[:, 3]
+
+# find the areas of all the boxes
+areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+
+# take the indexes of order the probability score in descending order
+# non maximum suppression
+order = order.argsort()[::-1]
+keep = []
+
+while (order.size > 0):
+  i = order[0] # take the 1st elt in roder and append to keep
+  keep.append(i)
+
+  xx1 = np.maximum(x1[i], x1[order[1:]])
+  yy1 = np.maximum(y1[i], y1[order[1:]])
+  xx2 = np.minimum(x2[i], x2[order[1:]])
+  yy2 = np.minimum(y2[i], y2[order[1:]])
+
+  w = np.maximum(0.0, xx2 - xx1 + 1)
+  h = np.maximum(0.0, yy2 - yy1 + 1)
+
+  inter = w * h
+  ovr = inter / (areas[i] + areas[order[1:]] - inter)
+  inds = np.where(ovr <= nms_thresh)[0]
+  order = order[inds + 1]
+
+keep = keep[:n_train_post_nms] # while training/testing, use accordingly
+roi = roi[keep]
+print(len(keep), roi.shape)
+
+# Proposal Target layer
+# 1) Calculate IoUs
+n_sample = 128 # number of samples from roi
+pos_ratio = 0.25 # number of positive examples out of the n_samples
+pos_iou_thresh = 0.5 # min iou of region proposal with any ground truth object to consider it as positive label
+neg_iou_thresh_hi = 0.5 # iou 0~0.5 is considered as negative (0, background)
+neg_iou_thresh_lo = 0.0
+
+# fine the iou of each ground truth object with the region proposals
+ious = np.empty((len(roi), bbox.shape[0]), dtype = np.float32)
+ious.fill(0)
+
+for num1, i in enumerate(roi):
+  ya1, xa1, ya2, xa2 = i
+  anchor_area = (ya2 - ya1) * (xa2 - xa1)
+
+  for num2, j in enumerate(bbox):
+    yb1, xb1, yb2, xb2 = j
+    box_area = (yb2 - yb1) * (xb2 - xb1)
+    inter_x1 = max([xb1, xa1])
+    inter_y1 = max([yb1, ya1])
+    inter_x2 = min([xb2, xa2])
+    inter_y2 = min([yb2, ya2])
+
+    if (inter_x1 < inter_x2) and (inter_y1 < inter_y2):
+      inter_area = (inter_y2 - inter_y1) * (inter_x2 - inter_x1)
+      iou = inter_area / (anchor_area + box_area - inter_area)
+    else:
+      iou = 0
+    ious[num1, num2] = iou
+
+print(ious.shape)
+
+# find out whick ground truth has high IoU for each region proposal
+# also find the maximum IoU
+gt_assignment = ious.argmax(axis=1)
+max_iou = ious.max(axis=1)
+
+print(gt_assignment)
+print(max_iou)
+
+# assign the labels to each proposal
+gt_roi_label = labels[gt_assignment]
+print(gt_roi_label)
+
+# 2) Select foreground(positive) samples
+# select the foreground rois as pre the pos_iou_thresh
+# and n_sample x pos_ratio (128 x 0.25 = 32) foreground samples
+pos_roi_per_image = 32
+pos_index = np.where(max_iou >= pos_iou_thresh)[0]
+pos_roi_per_this_image = int(min(pos_roi_per_image, pos_index.size))
+
+if pos_index.size > 0:
+  pos_index = np.random.choice(
+      pos_index, size=pos_roi_per_this_image, replace=False)
+  
+print(pos_roi_per_this_image)
+print(pos_index)
+
+# 3) Select background(negative) samples
+# similarly we do for negative(background) region proposals
+neg_index = np.where((max_iou < neg_iou_thresh_hi) &
+                     (max_iou >= neg_iou_thresh_lo))[0]
+neg_roi_per_this_image = n_sample - pos_roi_per_this_image
+neg_roi_per_this_image = int(min(neg_roi_per_this_image, neg_index.size))
+
+if neg_index.size > 0:
+  neg_index = np.random.choice(
+    neg_index, size = neg_roi_per_this_image, replace=False)
+  
+print(neg_roi_per_this_image)
+print(neg_index)
+
+# 4) Visualization
+# display RoI samples with positive
+img_clone = np.copy(img)
+
+for i in range(pos_roi_per_this_image):
+  x1, y1, x2, y2 = roi[pos_index[i]].astype(int)
+  cv2.rectangle(img_clone, (x1, y1), (x2, y2), color=(255,255,255),
+                thickness=3)
+  
+for i in range(len(bbox)):
+  cv2.rectangle(img_clone, (bbox[i][0], bbox[i][1]), (bbox[i][2], bbox[i][3]), 
+                color = (0, 255, 0), thickness=3)
+
+plt.imshow(img_clone)
+plt.show()
+
+# display RoI samples with negative
+img_clone = np.copy(img)
+
+plt.figure(figsize=(9, 6))
+
+for i in range(neg_roi_per_this_image):
+  x1, y1, x2, y2 = roi[neg_index[i]].astype(int)
+  cv2.rectangle(img_clone, (x1, y1), (x2, y2), color=(255, 255, 255),
+                thickness=3)
+  
+for i in range(len(bbox)):
+  cv2.rectangle(img_clone, (bbox[i][0], bbox[i][1]), (bbox[i][2], bbox[i][3]), 
+                color = (0, 255, 0), thickness=3)
+  
+plt.imshow(img_clone)
+plt.show()
+
+
+# 5) Gather positive/negative samples
+# now we gather positive samples index and negative samples index
+# their respective labels and region proposals
+keep_index = np.append(pos_index, neg_index)
+gt_roi_labels = gt_roi_label[keep_index]
+gt_roi_labels[pos_roi_per_this_image:] = 0 # negative labels => 0
+sample_roi = roi[keep_index]
+print(sample_roi.shape)
+
+# pick the ground truth objects for these sample_roi and
+# later parameterized as we have done while assigning locations to 
+# anchor boxes
+bbox_for_sampled_roi = bbox[gt_assignment[keep_index]]
+print(bbox_for_sampled_roi.shape)
+
+width = sample_roi[:, 2] - sample_roi[:, 0]
+height = sample_roi[:, 3] - sample_roi[:, 1]
+ctr_x = sample_roi[:, 0] + 0.5 * width
+ctr_y = sample_roi[:, 1] + 0.5 * height
+
+base_width = bbox_for_sampled_roi[:, 2] - bbox_for_sampled_roi[:, 0]
+base_height = bbox_for_sampled_roi[:, 3] - bbox_for_sampled_roi[:, 1]
+base_ctr_x = bbox_for_sampled_roi[:, 0] + 0.5 * base_width
+base_ctr_y = bbox_for_sampled_roi[:, 1] + 0.5 * base_height
+
+# transform anchor boxes
+eps = np.finfo(height.dtype).eps
+height = np.maximum(height, eps)
+width = np.maximum(width, eps)
+
+dx = (base_ctr_x - ctr_x) / width
+dy = (base_ctr_y - ctr_y) / height
+dw = np.log(base_width / width)
+dh = np.log(base_height / height)
+
+gt_roi_locs = np.vstack((dx, dy, dw, dh)).transpose()
+print(gt_roi_locs.shape)
+
+# RoI pooling
+# 1) Concatenate labels with bbox coordinates
+# Take out the features of 128 ROI samples and 
+# use max pooling to adjust to the same size, H=7, W=7 (ROI Pooling)
+rois = torch.from_numpy(sample_roi).float()
+roi_indices = 0 * np.ones((len(rois),), dtype=np.int32)
+roi_indices = torch.from_numpy(roi_indices).float()
+print(rois.shape, roi_indices.shape)
+
+indices_and_rois = torch.cat([roi_indices[:, None], rois], dim=1)
+xy_indices_and_rois = indices_and_rois[:, [0, 2, 1, 4, 3]]
+indices_and_rois = xy_indices_and_rois.contiguous()
+print(xy_indices_and_rois.shape)
+
+# 2) RoI pooling
+size = (7, 7)
+adaptive_max_pool = nn.AdaptiveMaxPool2d(size[0], size[1])
+
+output = []
+rois = indices_and_rois.data.float()
+rois[:, 1:].mul_(1/16.0) # sub-sampling ratio
+rois = rois.long()
+num_rois = rois.size(0)
+
+for i in range(num_rois):
+  roi = rois[i]
+  im_idx = roi[0]
+  im = output_map.narrow(0, im_idx, 1)[..., roi[1]:(roi[3]+1), roi[2]:(roi[4]+1)]
+  tmp = adaptive_max_pool(im)
+  output.append(tmp[0])
+
+output = torch.cat(output, 0)
+
+print(output.size())
+
+# Visualize the first 5 ROI's feature map (for each feature map, only show the 1st channel of d=512)
+fig=plt.figure(figsize=(12, 4))
+figNo = 1
+for i in range(5):
+    roi = rois[i]
+    im_idx = roi[0]
+    im = output_map.narrow(0, im_idx, 1)[..., roi[2]:(roi[4]+1), roi[1]:(roi[3]+1)]
+    tmp = im[0][0].detach().cpu().numpy()
+    fig.add_subplot(1, 5, figNo) 
+    plt.imshow(tmp, cmap='gray')
+    
+    figNo +=1
+plt.show()
+
+# Reshape the tensor so that we can pass it through the feed forward layer.
+k = output.view(output.size(0), -1)
+print(k.shape) # 25088 = 7*7*512
+
+
+# Fast R-CNN
+# 1) Classifier and Bounding box regressor
+# 128 boxes + features (7x7x512) of ROI samples are sent to 
+# Detection network to predict the objects bounding box and clas of the input image
+roi_head_classifier = nn.Sequential(*[nn.Linear(25088, 4096), nn.Linear(4096, 4096)]).to(DEVICE)
+cls_loc = nn.Linear(4096, 2 * 4).to(DEVICE) # 1 class, 1 background, 4 coordiinates
+cls_loc.weight.data.normal_(0, 0.01)
+cls_loc.bias.data.zero_()
+
+score = nn.Linear(4096, 2).to(DEVICE) # 1 class, 1 background
+
+# passing the output of roi pooling to RoI head
+k = roi_head_classifier(k.to(DEVICE))
+roi_cls_loc = cls_loc(k)
+roi_cls_score = score(k)
+
+print(roi_cls_loc.shape, roi_cls_score.shape)
+
+# Calculate the loss of Fast RCNN based on the gt bboxes and features (h, w, d=512) 
+# corresponding to these 128 ROIs
+# predicted
+print(roi_cls_loc.shape)
+print(roi_cls_score.shape)
+
+#actual
+print(gt_roi_locs.shape)
+print(gt_roi_labels.shape)
+
+# 2) Classification loss
+# Converting ground truth to torch variable
+gt_roi_loc = torch.from_numpy(gt_roi_locs)
+gt_roi_label = torch.from_numpy(np.float32(gt_roi_labels)).long()
+print(gt_roi_loc.shape, gt_roi_label.shape)
+
+#Classification loss
+roi_cls_loss = F.cross_entropy(roi_cls_score.cpu(), gt_roi_label.cpu(), ignore_index=-1)
+
+# 3) Regression loss
+# regression loss
+n_sample = roi_cls_loc.shape[0]
+roi_loc = roi_cls_loc.view(n_sample, -1, 4)
+print(roi_loc.shape)
+
+roi_loc = roi_loc[torch.arange(0, n_sample).long(), gt_roi_label]
+print(roi_loc.shape)
+
+# for regression we use smooth l1 loss as defined in the Fast R-CNN paper
+pos = gt_roi_label > 0
+mask = pos.unsqueeze(1).expand_as(roi_loc)
+print(mask.shape)
+
+# take those bounding boxes which have positive labels
+mask_loc_preds = roi_loc[mask].view(-1, 4)
+mask_loc_targets = gt_roi_loc[mask].view(-1, 4)
+print(mask_loc_preds.shape, mask_loc_targets.shape)
+
+x = torch.abs(mask_loc_targets.cpu() - mask_loc_preds.cpu())
+roi_loc_loss = ((x < 1).float() * 0.5 * x ** 2) + ((x >= 1).float() * (x - 0.5))
+print(roi_loc_loss.sum())
+
+# 4) Multi-task loss
+roi_lambda = 10.
+roi_loss = roi_cls_loss + (roi_lambda * roi_loc_loss)
+print(roi_loss)
+
+total_loss = rpn_loss + roi_loss
+print(total_loss)
