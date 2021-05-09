cb4d39e36ca21fa8f71e2d41acd52aece4b4011a
ajw1587 <dlsdnghkd0503@naver.com>
2021-05-09 23:29:22 +0900

    Add Coding Test

--- Coding_Test/Python/Quiz/future_city.py
+++ Coding_Test/Python/Quiz/future_city.py
@@ -30,4 +30,20 @@ for _ in range(m):
   graph[x][y] = 1
   graph[y][x] = 1
 
-# 
+# 거쳐갈 노드 X와 최종 목적지 노드 K 입력받기
+x, k = map(int, input().split())
+
+# 점화식에 따라 플로이드 워셜 알고리즘을 수행
+for k in range(1, n + 1):
+  for a in range(1, n + 1):
+    for b in range(1, n + 1):
+      graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+    
+# 수행된 결과를 출력
+distance = graph[1][k] + graph[k][x]
+
+# 도달할 수 없는 경우, -1을 출력
+if distance >= INF:
+  print('-1')
+else:
+  print(distance)
