28f9520f56b4482291d54305ef43e7190993cab8
ajw1587 <dlsdnghkd0503@naver.com>
2021-05-11 17:54:39 +0900

    Add Sieve of Eratosthenes

---
+++ Coding_Test/Python/Algorithm/17_upgrade_prime_number.py
@@ -0,0 +1,10 @@
+# 약수의 성질
+# 모든 약수가 가운데 약수를 기준으로 곱셈 연산에 대해 대칭을 이루는 것을 알 수 있다.
+# 소수 판별
+import math
+
+def is_prime_number(x):
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
