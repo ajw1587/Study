bf0daf7f16e28f7885fe22a946b6227f30818747
ajw1587 <dlsdnghkd0503@naver.com>
2021-05-14 22:28:49 +0900

    Add Interval Sum

--- Coding_Test/Python/Algorithm/20_interval_sum.py
+++ Coding_Test/Python/Algorithm/20_interval_sum.py
@@ -8,3 +8,16 @@
 # 주어진 배열 = 10 / 20 / 30 / 40 / 50
 # 접두사 배열(P) = 0 / 10 / 30 / 60 / 100 / 150
 # 구간합: P[Right] - P[Left - 1]
+
+n = 5
+data = [10, 20, 30, 40, 50]
+
+sum_value = 0
+prefix_sum = [0]
+for i in data:
+    sum_value += i
+    prefix_sum.append(sum_value)
+
+left = 3
+right = 4
+print(prefix_sum[right] - prefix_sum[left - 1])
