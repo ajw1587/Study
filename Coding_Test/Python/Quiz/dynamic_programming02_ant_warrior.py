c22efc4f284eefdfb35f0061f1b9bb7bfcbb78a9
ajw1587 <dlsdnghkd0503@naver.com>
2021-05-19 23:47:14 +0900

    Add

--- Coding_Test/Python/Quiz/dynamic_programming02_ant_warrior.py
+++ Coding_Test/Python/Quiz/dynamic_programming02_ant_warrior.py
@@ -12,7 +12,7 @@ array = list(map(int, input().split()))
 dp = [0] * 100
 dp[0] = array[0]
 dp[1] = max(array[0], array[1])
-for i in range(n):
+for i in range(2, n):
   dp[i] = max(dp[i - 1], dp[i - 2] + array[i])
 
 print(dp[-1])
