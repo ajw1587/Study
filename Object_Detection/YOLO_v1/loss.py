e3efaeb4fdfd7212c7eba619af53dd05f2192e13
ajw1587 <dlsdnghkd0503@naver.com>
2021-05-24 01:03:23 +0900

    Add YOLO_v1 Loss

--- Object_Detection/YOLO_v1/loss.py
+++ Object_Detection/YOLO_v1/loss.py
@@ -1,1 +1,1 @@
-# https://medium.com/curg/you-only-look-once-%EB%8B%A4-%EB%8B%A8%EC%A7%80-%ED%95%9C-%EB%B2%88%EB%A7%8C-%EB%B3%B4%EC%95%98%EC%9D%84-%EB%BF%90%EC%9D%B4%EB%9D%BC%EA%B5%AC-bddc8e6238e2
+# https://brunch.co.kr/@kmbmjn95/35
